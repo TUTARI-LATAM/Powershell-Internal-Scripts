2f2ea92e34573f9af7ec94ff51ef54e15f717922
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-24 11:38:40 -0600

    Update Add_exceptions_awarness_campaign.ps1

--- Add_exceptions_awarness_campaign.ps1
+++ Add_exceptions_awarness_campaign.ps1
@@ -1,16 +1,5 @@
 Install-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
 
-#Conexión a Azure Active Directory
-Connect-AzureAD
-
-#Obtener todos los usuarios de la organización
-$users = Get-AzureADUser -All $true | Select-Object UserPrincipalName
-
 #Agregar remitentes seguros para cada usuario
-foreach ($user in $users) {
-    Set-MailboxJunkEmailConfiguration -Identity $user.UserPrincipalName -TrustedSendersAndDomains @{Add="pishing.tutari.net","noreply@pishing.tutari.net"}
-}
-
-
 Get-Mailbox -ResultSize Unlimited | Set-MailboxJunkEmailConfiguration -TrustedSendersAndDomains "pishing.tutari.net","noreply@pishing.tutari.net"
