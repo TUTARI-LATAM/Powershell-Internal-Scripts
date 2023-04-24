02da730882e992eece912177e3d8927a8b73db1e
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-24 11:10:45 -0600

    Add files via upload

---
+++ Add_exceptions_awarness_campaign.ps1
@@ -0,0 +1,10 @@
+﻿#Conexión a Azure Active Directory
+Connect-AzureAD
+
+#Obtener todos los usuarios de la organización
+$users = Get-AzureADUser -All $true | Select-Object UserPrincipalName
+
+#Agregar remitentes seguros para cada usuario
+foreach ($user in $users) {
+    Set-MailboxJunkEmailConfiguration -Identity $user.UserPrincipalName -TrustedSendersAndDomains @{Add="pishing.tutari.net","noreply@pishing.tutari.net"}
+}
