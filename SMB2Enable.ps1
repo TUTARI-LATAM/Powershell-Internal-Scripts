098b8892fff46855367c8d85a1c9bbf63835ecb8
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-25 16:21:25 -0600

    Update SMB2Enable.ps1

--- SMB2Enable.ps1
+++ SMB2Enable.ps1
@@ -1,12 +1,16 @@
 ﻿# Enable SMB2 protocol
 Set-SmbServerConfiguration -EnableSMB2Protocol $true
-Set-SmbClientConfiguration -EnableSMB2Protocol $true
+
 
 # Disable SMB1 protocol
 Set-SmbServerConfiguration -EnableSMB1Protocol $false
-Set-SmbClientConfiguration -EnableSMB1Protocol $false
+
 
 #
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "SMB1" -Type DWORD -Value 0
-Get-SmbConnection
 
+
+
+sc.exe qc lanmanworkstation
+sc.exe config lanmanworkstation depend= bowser/mrxsmb10/mrxsmb20/nsi
+sc.exe config mrxsmb20 start= auto
