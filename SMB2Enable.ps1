63719477bc48c7db4ff237962ab230a877a6f4f1
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-25 15:56:03 -0600

    Update SMB2Enable.ps1

--- SMB2Enable.ps1
+++ SMB2Enable.ps1
@@ -1,7 +1,12 @@
 ﻿# Enable SMB2 protocol
 Set-SmbServerConfiguration -EnableSMB2Protocol $true
+Set-SmbClientConfiguration -EnableSMB2Protocol $true
 
 # Disable SMB1 protocol
 Set-SmbServerConfiguration -EnableSMB1Protocol $false
+Set-SmbClientConfiguration -EnableSMB1Protocol $false
+
 #
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "SMB1" -Type DWORD -Value 0
+Get-SmbConnection
+
