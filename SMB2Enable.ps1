37941079a392b901b1e90596c0efabe98a40828e
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-25 15:39:34 -0600

    Add files via upload

---
+++ SMB2Enable.ps1
@@ -0,0 +1,7 @@
+﻿# Enable SMB2 protocol
+Set-SmbServerConfiguration -EnableSMB2Protocol $true
+
+# Disable SMB1 protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false
+#
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "SMB1" -Type DWORD -Value 0
