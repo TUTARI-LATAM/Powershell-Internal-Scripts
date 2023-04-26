12d1920e22a3cdfc741ab26c7fb166ff95b730ca
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-26 15:22:00 -0600

    Add files via upload

---
+++ PermisosSharedFolders.ps1
@@ -0,0 +1,16 @@
+﻿Get-WmiObject -Class Win32_Share | Where-Object {$_.Name -like "*$*"} | ForEach-Object {
+    $share = [WMIClass]($_.__CLASS)
+    $params = @{
+        Name = $_.Name
+        Path = $_.Path
+        Type = $_.Type
+        Description = $_.Description
+    }
+    $params["MaximumAllowed"] = $_.MaximumAllowed
+    $params["Password"] = $_.Password
+    $params["Caption"] = $_.Caption
+    $params["AllowMaximum"] = $_.AllowMaximum
+    $params["AccessMask"] = 2032127
+    $params["Account"] = "DELOROCR\Domain Admins"
+    $share.psbase.InvokeMethod("SetShareInfo", @($params.Name, $params.Path, $params.Type, $params.Description, $params.Password, $null, $params.MaximumAllowed))
+}
