3ac2ee3a7d11a95b67913bbad9b166500e3d2fbd
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-26 15:49:36 -0600

    Update PermisosSharedFolders.ps1

--- PermisosSharedFolders.ps1
+++ PermisosSharedFolders.ps1
@@ -1,4 +1,9 @@
 Get-SmbShare | Where-Object {$_.Name -like "*$*"} | ForEach-Object {
-    Set-SmbShare -Name $_.Name -FullAccess "DELOROCR\Domain Admins"
+    $shareName = $_.Name
+    $acl = Get-Acl $_.Path
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("DELOROCR\Domain Admins","FullControl","Allow")
+    $acl.SetAccessRule($rule)
+    $sec = $acl.GetSecurityDescriptorSddlForm("All")
+    Set-SmbShareAccess -Name $shareName -AccessRight Full -AccessControlType Allow -AccountName "DELOROCR\Domain Admins" -CimSession (Get-CimSession) -SecurityDescriptorSddl $sec
 }
 Get-SmbShare | Where-Object { $_.Name -notlike "*$*" } | Remove-SmbShare
