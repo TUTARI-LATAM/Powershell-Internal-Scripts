73f60da68f18bab5ed56229ba0eec7c990c4427a
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-26 16:13:11 -0600

    Update PermisosSharedFolders.ps1

--- PermisosSharedFolders.ps1
+++ PermisosSharedFolders.ps1
@@ -1,9 +1,5 @@
-Get-SmbShare | Where-Object {$_.Name -like "*$*"} | ForEach-Object {
-    $shareName = $_.Name
-    $acl = Get-Acl $_.Path
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("DELOROCR\Domain Admins","FullControl","Allow")
-    $acl.SetAccessRule($rule)
-    $sec = $acl.GetSecurityDescriptorSddlForm("All")
-    Set-SmbShareAccess -Name $shareName -AccessRight Full -AccessControlType Allow -AccountName "DELOROCR\Domain Admins" -CimSession (Get-CimSession) -SecurityDescriptorSddl $sec
-}
+Get-SmbShare | Where-Object {$_.Name -like "*$*"} | Get-SmbShareAccess | Where-Object {$_.AccountName -eq "Everyone"} | ForEach-Object {  
+    Revoke-SmbShareAccess -name $_.name -AccountName $_.AccountName -Force}  
+Get-SmbShare | Where-Object {$_.Name -like "*$*"} | Get-SmbShareAccess | Where-Object {$_.AccountName -eq "DELOROCR\Domain Admins"} | ForEach-Object {  
+    Grant-SmbShareAccess -name $_.name -AccountName $_.AccountName -Force}  
 Get-SmbShare | Where-Object { $_.Name -notlike "*$*" } | Remove-SmbShare
