7455fd326b52cd3ecde14c5bcb288e64d73ac938
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-26 17:07:03 -0600

    Update SMB2Enable.ps1

--- SMB2Enable.ps1
+++ SMB2Enable.ps1
@@ -56,4 +56,13 @@ else {
     sc.exe config mrxsmb20 start= auto
     Write-Host "El servicio LanmanWorkstation se configuró correctamente."
 }
+#PowerShell script to remove Internet Explorer
+#Check If IE is Installed
+$check = Get-WindowsOptionalFeature -Online | Where-Object {$_.FeatureName -eq "Internet-Explorer-Optional-amd64"}
+If ($check.State -ne "Disabled")
+{
+#Remove Internet Explorer
+Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 -Online -NoRestart | Out-Null
+}
+
 
