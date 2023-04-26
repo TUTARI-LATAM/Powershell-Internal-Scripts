56744529d8163895a55e3415f577fb7ef4dff0dc
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-26 15:38:52 -0600

    Update PermisosSharedFolders.ps1

--- PermisosSharedFolders.ps1
+++ PermisosSharedFolders.ps1
@@ -1,16 +1,5 @@
-﻿Get-WmiObject -Class Win32_Share | Where-Object {$_.Name -like "*$*"} | ForEach-Object {
-    $share = [WMIClass]($_.__CLASS)
-    $params = @{
-        Name = $_.Name
-        Path = $_.Path
-        Type = $_.Type
-        Description = $_.Description
-    }
-    $params["MaximumAllowed"] = $_.MaximumAllowed
-    $params["Password"] = $_.Password
-    $params["Caption"] = $_.Caption
-    $params["AllowMaximum"] = $_.AllowMaximum
-    $params["AccessMask"] = 2032127
-    $params["Account"] = "DELOROCR\Domain Admins"
-    $share.psbase.InvokeMethod("SetShareInfo", @($params.Name, $params.Path, $params.Type, $params.Description, $params.Password, $null, $params.MaximumAllowed))
+Get-SmbShare | Where-Object {$_.Name -like "*$*"} | ForEach-Object {
+    $shareName = $_.Name
+    Set-SmbShare -Name $shareName -FolderParameter $_.Path -FullAccess "DELOROCR\Domain Admins"
 }
+Get-SmbShare | Where-Object { $_.Name -notlike "*$*" } | Remove-SmbShare
