66f151208f1c30df2740f4878d56359708bf1a7c
TUTARI-LATAM <60111745+TUTARI-LATAM@users.noreply.github.com>
2023-04-26 16:45:51 -0600

    Update PermisosSharedFolders.ps1

--- PermisosSharedFolders.ps1
+++ PermisosSharedFolders.ps1
@@ -1,5 +1,4 @@
 Get-SmbShare | Where-Object {$_.Name -like "*$*"} | Get-SmbShareAccess | Where-Object {$_.AccountName -eq "Everyone"} | ForEach-Object {  
     Revoke-SmbShareAccess -name $_.name -AccountName $_.AccountName -Force}  
-Get-SmbShare | Where-Object {$_.Name -like "*$*"} | Get-SmbShareAccess | Where-Object {$_.AccountName -eq "DELOROCR\Domain Admins"} | ForEach-Object {  
-    Grant-SmbShareAccess -name $_.name -AccountName $_.AccountName -Force}  
+Get-SmbShare | ForEach-Object { Grant-SmbShareAccess -Name $_.Name -AccountName "DELOROCR\Domain Admins" -AccessRight Full -Force }
 Get-SmbShare | Where-Object { $_.Name -notlike "*$*" } | Remove-SmbShare
